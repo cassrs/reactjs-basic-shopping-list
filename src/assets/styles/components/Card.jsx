aceb1c973f4ac92a0a3abd6d340138805f985413
palaarda <gametime.plz@outlook.com>
2024-01-07 08:14:35 +0300

    card-a-update

--- src/assets/styles/components/Card.jsx
+++ src/assets/styles/components/Card.jsx
@@ -4,23 +4,25 @@ import Stars from "./Stars";
 
 const Card = (props) => {
     return (
-        <div class="col mb-5">
-            <div class="card h-100">
-                {props.badge}
-                <img class="card-img-top" src={props.image} alt="..." />
-                <div class="card-body p-4">
-                    <div class="text-center">
-                        <h5 class="fw-bolder">{props.title}</h5>
-                        {props.stars}
-                        {props.priceLine}
-                        {props.price}
+        <a href="#" className="cards">
+            <div class="col mb-5">
+                <div class="card h-100">
+                    {props.badge}
+                    <img class="card-img-top" src={props.image} alt="..." />
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <h5 class="fw-bolder">{props.title}</h5>
+                            {props.stars}
+                            {props.priceLine}
+                            {props.price}
+                        </div>
+                    </div>
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
                     </div>
                 </div>
-                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                </div>
             </div>
-        </div>
+        </a>
     )
 }
 export default Card;
